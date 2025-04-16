c5a8f08e49b8aae295547a50196b00287f8685fe
maxalanclark <maxaclark@gmail.com>
2025-04-16 16:24:35 -0700

    - Defined square type enum

--- scripts/Square.gd
+++ scripts/Square.gd
@@ -3,3 +3,12 @@ extends Node2D
 
 # going to have to figure out how to track the interior spots
 var num = -1
+var type : Type = Type.Undefined
+
+enum Type {
+	Undefined,
+	Property,
+	Utility,
+	Chance,
+	Corner
+}
